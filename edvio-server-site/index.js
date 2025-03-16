451ed4eccbe7cb885b0937e4f6393084364b5d63
Johora99 <fatimatuzjohora1999@gmail.com>
2025-03-16 23:52:19 +0600

    server site setup

---
+++ edvio-server-site/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+  res.send('Edvio server is running');
+})
+
+app.listen(port,()=>{
+  console.log(`Server is running on PORT : ${port}`)
+})
+
+// DB_USER : edVio
+// DB_PASSWORD : ZjjcxkvD0uusSqsL
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://edVio:ZjjcxkvD0uusSqsL@cluster0.3oeok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+  
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
