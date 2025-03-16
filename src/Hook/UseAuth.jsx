e4a52c266169e15b7c21150a3461de84ab65be7b
Johora99 <fatimatuzjohora1999@gmail.com>
2025-03-16 22:07:31 +0600

    server site added

--- src/Hook/UseAuth.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../AuthProvider/AuthProvider';
-
-const UseAuth = () => { 
-    const auth = useContext(AuthContext)
-    return auth   
-};
-
-export default UseAuth;
