08215b3c38f13efa2586e6b6d50eefd01793d43e
Sharmin Akter <sharminshopna149@gmail.com>
2025-03-21 21:03:22 +0600

    added dashboard

--- edvio-client-site/src/DashboardLayOut/DashboardLayOut.jsx
+++ edvio-client-site/src/DashboardLayOut/DashboardLayOut.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import "../DashboardLayOut/DashboardLayOut.css"
 import { NavLink, Outlet } from 'react-router-dom';
 import { SiDiscourse } from "react-icons/si";
+ import icon from "../../src/assets/icon.png"
+ import "../index.css"
+
 
 const DashboardLayOut = () => {
+    // console.log(icon)
+    // const icon = "https://i.ibb.co.com/JWJTQ30X/ev.png"
     return (
         <>
+        
             {/* <h1 className='text-3xl'>This is Dashboard</h1> */}
             <div className='flex'>
                 <div className='w-44 md:w-64 min-h-screen tealGreen pt-12'>
-                    <div></div>
+                    <div className='flex gap-4'>
+                       <img src={icon }alt="" className='w-8 h-8 bg-white rounded-full'/>
+                        <h1 className='text-2xl text-white Logo'>EdVio</h1>
+                    </div>
                     <ul className='p-4 text-gray-100 space-y-2'>
                         <li>
                             <NavLink to='/dashboard'
