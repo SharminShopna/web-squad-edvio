2a7460f83dbc8065738fa67788601b85658f7562
pgAminul <businessbyaminul@gmail.com>
2025-03-26 00:27:44 +0800

    added login page functionality if users try login by worng password 3 times they account will ban for 1 hour

--- edvio-client-site/src/Hooks/useAxiosSecure.jsx
+++ edvio-client-site/src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "https://edvio-server-site.vercel.app",
+  // baseURL: "https://edvio-server-site.vercel.app",
+  baseURL: "http://localhost:5000",
 });
 export default function useAxiosSecure() {
   return axiosSecure;
