ce65d524a59e4373520e4e8fa1b2c6e15ebc4390
Johora99 <fatimatuzjohora1999@gmail.com>
2025-04-05 21:59:12 +0600

    dashboard update

--- edvio-client-site/src/Components/Dashboard/AdminDashBoard/DashBoardHome.jsx
+++ edvio-client-site/src/Components/Dashboard/AdminDashBoard/DashBoardHome.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
+import TotalInfoCard from '../ShareComponent/TotalInfoCard'
+import { FaUsersGear } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+import { FeesChart } from '../ShareComponent/FeesChart';
 
 export default function DashBoardHome() {
+  const dashBoardData = {
+    
+  "totalStudents": 3280,
+  "studentGrowthRate": 80,
+  "newStudents": 245,
+  "newStudentsGrowthRate": 70,
+  "totalCourses": 28,
+  "courseGrowthRate": -10,
+  "feesCollected": 25160,
+  "feesGrowthRate": -12
+
+
+  }
   return (
     <div className='w-full'>
       <div className='m-10'>
-         
+         <div className='grid grid-cols-1 lg:grid-cols-12 gap-10'>
+           <div className='lg:col-span-5'>
+             <div className='grid grid-cols-1 lg:grid-cols-2 gap-10'>
+              <div className='bg-neutral rounded-lg'>
+                <TotalInfoCard icon={<FaUsersGear />} title={"Total Students"} totalCount={dashBoardData?.totalStudents} rate={dashBoardData?.studentGrowthRate}></TotalInfoCard>
+              </div>
+              <div className='bg-neutral rounded-lg'>
+               <TotalInfoCard icon={<FaUser />} title={"New Students"} totalCount={dashBoardData?.newStudents} rate={dashBoardData?.newStudentsGrowthRate}></TotalInfoCard>
+              </div>
+              <div className='bg-neutral rounded-lg'>
+                 <TotalInfoCard icon={<FaBook />} title={"Total Course"} totalCount={dashBoardData?.totalCourses} rate={dashBoardData?.courseGrowthRate}></TotalInfoCard>
+              </div>
+              <div className='bg-neutral rounded-lg'>
+               <TotalInfoCard icon={<RiMoneyDollarCircleFill />} title={"Fees Collection"} totalCount={dashBoardData?.feesCollected} rate={dashBoardData?.feesGrowthRate}></TotalInfoCard>
+              </div>
+             </div>
+           </div>
+           <div className='col-span-7 bg-neutral rounded-lg'>
+        
+           </div>
+         </div>
       </div>
     </div>
   )
