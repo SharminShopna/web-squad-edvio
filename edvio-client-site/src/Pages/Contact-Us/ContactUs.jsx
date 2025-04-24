a51b0aa3ee8f041bc8b646ffeed3cdd0837ce580
Sharmin Akter <sharminshopna149@gmail.com>
2025-04-24 20:26:16 +0600

    added email

--- edvio-client-site/src/Pages/Contact-Us/ContactUs.jsx
+++ edvio-client-site/src/Pages/Contact-Us/ContactUs.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import bg from "../../assets/contactUs/contact-bg.jpg"
 import Contact from '@/Components/Contact';
+import { MdEmail, MdOutlinePhoneInTalk } from 'react-icons/md';
+import { FaLocationDot } from 'react-icons/fa6';
 
 const ContactUs = () => {
     return (
@@ -20,11 +22,11 @@ const ContactUs = () => {
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
                         {/* Phone & WhatsApp */}
                         <div className="shadow-lg p-4 rounded-xl bg-neutral border border-base-content border-dashed">
-                            <h2 className="text-xl font-semibold mb-4">📞 PHONE & WHATSAPP</h2>
+                            <h2 className="text-xl font-semibold mb-4 flex items-center gap-2 "><MdOutlinePhoneInTalk /> PHONE & WHATSAPP</h2>
                             <div className="space-y-2">
                                 <div className='flex gap-1'>
-                                <img src={bg} alt="" className='h-12 w-12 rounded-full' />
-                                <a href="tel:11111" className="block text-blue-600 hover:underline">11111</a>
+                                <img src={bg} alt="" className='h-8 w-8 rounded-full' />
+                                <a href="tel:11111" className="block text-blue-600 hover:underline text-2xl">11111</a>
                                 </div>
                                 <a href="tel:11111" className="block text-blue-600 hover:underline">11111</a>
                                 <a href="tel:11111" className="block text-blue-600 hover:underline">11111</a>
@@ -36,20 +38,20 @@ const ContactUs = () => {
 
                         {/* Email */}
                         <div className="shadow-lg p-4 rounded-xl bg-neutral border border-base-content border-dashed">
-                            <h2 className="text-xl font-semibold mb-4">📧 EMAIL</h2>
+                            <h2 className="text-xl font-semibold mb-4 flex items-center gap-2"><MdEmail /> EMAIL</h2>
                             <div className="space-y-2">
-                                <p>example1@gmail.com</p>
-                                <p>example2@gmail.com</p>
-                                <p>example3@gmail.com</p>
-                                <p>example4@gmail.com</p>
-                                <p>example5@gmail.com</p>
-                                <p>example6@gmail.com</p>
+                                <p>fatimatuzjohora1999@gmail.com</p>
+                                <p>aminul254237@gmail.com</p>
+                                <p>shawonk4@gmail.com</p>
+                                <p>mahin1575@gmail.com</p>
+                                <p>ismailhosen8757@gmail.com</p>
+                                <p>sharminshopna149@gmail.com</p>
                             </div>
                         </div>
 
                         {/* Address */}
                         <div className="shadow-lg p-4 rounded-xl bg-neutral border border-base-content border-dashed">
-                            <h2 className="text-xl font-semibold mb-4">🏠 ADDRESS</h2>
+                            <h2 className="text-xl font-semibold mb-4 flex items-center gap-2"><FaLocationDot /> ADDRESS</h2>
                             <div className="space-y-2">
                                 <p>123 Main Street</p>
                                 <p>City Name</p>
